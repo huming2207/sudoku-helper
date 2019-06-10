5f14bc3bfc88b5988912ad6410ce48bf15577029
Jackson Ming Hu <huming2207@gmail.com>
2019-06-10 23:43:18 +1000

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Sudoku />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Sudoku from './components/Sudoku.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Sudoku
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Sudoku.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="sudoku">
+    <b-container class="mt-5">
+      <b-card>
+        <table>
+          <tr v-for="(row, rowIdx) in items" :key="row">
+            <td v-for="(col, colIdx) in row" :key="col">
+              <input :id="rowIdx.toString() + '-' + col.toString()"  max="5" style="width:15%" v-model.number="items[rowIdx][colIdx]" type="number">
+            </td>
+          </tr>
+        </table>
+      </b-card>
+      <b-card>
+        <div>
+          Enter size: <input type="number" style="width:15%" v-model.number="size">
+        </div>
+      </b-card>
+      <b-card>
+        <div>
+          Min value: <input type="number" style="width:15%" v-model.number="minVal">
+        </div>
+      </b-card>
+      <b-card>
+        <div>
+          Max value: <input type="number" style="width:15%" v-model.number="maxVal">
+        </div>
+      </b-card>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Sudoku',
+  data: function () {
+    return {
+      items: [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+      size: 3,
+      minVal: 0,
+      maxVal: 1,
+    }
+  },
+  methods: {
+    calculate() {
+      for(let row = 0; row < this.items.size; row += 1) {
+        for(let col = 0; col < this.items.size; col += 1) {
+          // items[row][col]
+        }
+      }
+    }
+  }
+}
+</script>
