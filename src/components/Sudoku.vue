cbfd128267ccb51fe2ecd6c4590e3a1722df443c
Jackson Ming Hu <huming2207@gmail.com>
2019-06-11 23:44:58 +1000

    Fix various typos

--- src/components/Sudoku.vue
+++ src/components/Sudoku.vue
@@ -3,21 +3,15 @@
     <b-container class="mt-5">
       <b-card>
         <table>
-          <tr v-for="(row, rowIdx) in items" :key="row">
-            <td v-for="(col, colIdx) in row" :key="col">
-              <input :id="rowIdx.toString() + '-' + col.toString()"  max="5" style="width:15%" v-model="items[rowIdx][colIdx]">
+          <tr v-for="(row, rowIdx) in items" :key="`row-${rowIdx}`">
+            <td v-for="(col, colIdx) in row" :key="`col-${colIdx}`">
+              <input :id="rowIdx.toString() + '-' + colIdx.toString()" :class="`row-${rowIdx}`" style="width:15%" type="text" v-model="items[rowIdx][colIdx]" v-on:input="refreshNum()">
             </td>
           </tr>
         </table>
       </b-card>
       <b-card>
-          Enter size: <input type="number" style="width:15%" v-model.number="size">
-      </b-card>
-      <b-card>
-          Min value: <input type="number" style="width:15%" v-model.number="minVal">
-      </b-card>
-      <b-card>
-          Max value: <input type="number" style="width:15%" v-model.number="maxVal">
+          Enter size: <input type="number" style="width:15%" v-model.number="size" v-on:input="generateItemArray()">
       </b-card>
       <b-card>
         <b-button variant="success" v-on:click="result = calculate()">Calculate</b-button>
@@ -36,13 +30,28 @@ export default {
     return {
       items: [["1", "2", ""], ["", "x", ""], ["", "", ""]],
       size: 3,
-      minVal: 0,
-      maxVal: 1,
-      possibleNum: ["1", "2", "3"],
+      possibleNum: new Set(),
       result: "(Unknown)"
     }
   },
   methods: {
+    generateItemArray() {
+      this.items = new Array(this.size);
+      for(let idx = 0; idx < this.items.length; idx += 1) {
+        this.items[idx] = new Array(this.size).fill('');
+      }
+    },
+
+    refreshNum() {
+      this.$forceUpdate(); // Yea it's shit but it works...
+      this.possibleNum.clear();
+      for(const row of this.items) {
+        for(const item of row) {
+          if(item && item !== "x") this.possibleNum.add(item);
+        }
+      }
+    },
+
     findX() {
       for(let row = 0; row < this.items.length; row += 1) {
         for(let col = 0; col < this.items.length; col += 1) {
@@ -55,43 +64,43 @@ export default {
 
     addToSet() {
       const plHolder = this.findX();
-      const boardSize = this.items.size;
+      const boardSize = this.items.length;
       let numSet = new Set();
       
       // Vertical position (Iterate rows)
       for(let idx = 0; idx < boardSize; idx += 1) {
         const currItem = this.items[idx][plHolder.col];
-        if(currItem && numSet.has(currItem) && currItem !== "x") numSet.add(currItem);
+        if(currItem && !numSet.has(currItem) && currItem !== "x") numSet.add(currItem);
       }
 
       // Horizontal position (Iterate columns)
       for(let idx = 0; idx < boardSize; idx += 1) {
         const currItem = this.items[plHolder.row][idx];
-        if(currItem && numSet.has(currItem) && currItem !== "x") numSet.add(currItem);
+        if(currItem && !numSet.has(currItem) && currItem !== "x") numSet.add(currItem);
       }
 
       // Forward-slash/Up position (row+, col-)
       for(let rowIdx = plHolder.row, colIdx = plHolder.col; rowIdx < boardSize && colIdx >= 0; rowIdx += 1, colIdx -= 1) {
         const currItem = this.items[rowIdx][colIdx];
-        if(currItem && numSet.has(currItem) && currItem !== "x") numSet.add(currItem);
+        if(currItem && !numSet.has(currItem) && currItem !== "x") numSet.add(currItem);
       }
 
       // Forward-slash/Down position (row-, col+)
       for(let rowIdx = plHolder.row, colIdx = plHolder.col; colIdx < boardSize && rowIdx >= 0; colIdx += 1, rowIdx -= 1) {
         const currItem = this.items[rowIdx][colIdx];
-        if(currItem && numSet.has(currItem) && currItem !== "x") numSet.add(currItem);
+        if(currItem && !numSet.has(currItem) && currItem !== "x") numSet.add(currItem);
       }
 
       // Backslash/Down position (row-, col-)
       for(let rowIdx = plHolder.row, colIdx = plHolder.col; colIdx < boardSize && rowIdx < boardSize; colIdx += 1, rowIdx += 1) {
         const currItem = this.items[rowIdx][colIdx];
-        if(currItem && numSet.has(currItem) && currItem !== "x") numSet.add(currItem);
+        if(currItem && !numSet.has(currItem) && currItem !== "x") numSet.add(currItem);
       }
 
       // Backslash/Down position (row+, col+)
       for(let rowIdx = plHolder.row, colIdx = plHolder.col; colIdx >= 0 && rowIdx >= 0; colIdx -= 1, rowIdx -= 1) {
         const currItem = this.items[rowIdx][colIdx];
-        if(currItem && numSet.has(currItem) && currItem !== "x") numSet.add(currItem);
+        if(currItem && !numSet.has(currItem) && currItem !== "x") numSet.add(currItem);
       }
 
       /* eslint-disable no-console */
